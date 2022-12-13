58c143144070d7a5df954481b1b1df579ac4919e
Breiz-sk <53227343+Breiz-sk@users.noreply.github.com>
2022-12-13 09:52:36 +0100

    Add files via upload

---
+++ Projet_C/BinDeci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BinDeci
+{ 
+	public class Esteban
+	{
+		public static int[] BinDeci(int[,] binIP)
+		{
+			int decimal = 0;
+
+			int[] tab = new int[4];
+
+			int nbLignes = binIP.GetLength(0);
+			int nbColonnes = binIP.GetLength(1);
+
+			for (int i = 0; i <= nbLignes; i++  )
+            {
+				for (int j = 0; i <= nbColonnes; i++)
+                {
+					if ( binIP[i,j] == 1)
+                    {
+						decimal += Math.Pow(2,j)
+					}
+                }
+				tab[i] = decimal;
+			}
+			return tab;
+
+		}
+	}
+}
