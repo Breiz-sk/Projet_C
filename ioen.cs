e4329640259daf21693f3215e4228c902e6479f1
ioenpm <118254047+ioenpm@users.noreply.github.com>
2022-12-13 10:08:13 +0100

    Add files via upload

---
+++ ioen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoenDECBIN
+{
+    class Program
+    {
+        public static int[] BinaireDecimale(int[,] binIP)
+        {
+            int[,] ipReseauBin = newint[4, 8];
+            char c =""
+            int
+                ligne = 0;
+
+
+            for (int col = 0; col < 4; col++)
+            {
+                for (int lign = 0; lign < 8; lign++)
+                       
+                {
+                    ipReseauBin[col, ligne] = 0
+                }
+            }
+
+            for (int colonne = 0; colonne < 4; colonne++)
+            {
+                char c  = Convert.ToString(binIP[colonne], 2);
+                foreach (char c in s)
+                {
+                    ipReseauBin[colonne, (c.Length - ligne)] = int.Parse(c);
+                    ligne++;
+                }
+            }
+        }
+    }
+}
