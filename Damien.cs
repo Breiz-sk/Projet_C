d9480b394c1ebc714f2b7e72ca2d5d27d65fe69e
Damienstg <118254007+Damienstg@users.noreply.github.com>
2022-12-13 10:09:08 +0100

    Add files via upload

---
+++ Damien.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_C
+{
+    class Damien
+    {
+    public static int[]SaisieIP(int[]newint
+        {
+            int[] tabNotes = new int[4];
+            for (int cpttab = 0; cpttab<= 3; cpttab++)
+            {
+                Console.Write("Saisir l'octet N°" + cpttab+":"  );
+                Console.Read();
+            }
+            Console.ReadKey();
+        }
+    }
+}
