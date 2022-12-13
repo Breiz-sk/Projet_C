6250f40b0383e2781e94ebdd8c1889549be9f5cf
GrabbyLR <118254116+GrabbyLR@users.noreply.github.com>
2022-12-13 10:07:55 +0100

    Add files via upload

---
+++ Ethan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_C
+{
+    class Ethan
+    {
+        public static int[,] CalculReseau(int[,] Ipbin, int[,] MasqueBin)
+        {
+            int[,] tabReseau = new int[4,8];
+            for (int octet = 0; octet <= 3;octet++)
+            {
+                for (int valBin=0;valBin<=7;valBin++)
+                {
+                    tabReseau[1] = Ipbin[1] + MasqueBin[1];
+                }
+            }
+        }
+    }    
+}
