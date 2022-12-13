8198e781683f1d2982c2d6a61225b4cb2a60c090
Hayrie <71726708+TiltproofHayrie@users.noreply.github.com>
2022-12-13 10:06:15 +0100

    Add files via upload

---
+++ salvar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_C
+{
+    internal class salvar
+    {
+        public static int[,] CalculMasque(int CIDR)
+        {
+            int[,] masqueTab = new int[4, 8];
+            for (int i = 0; i <= 4; i++) 
+            {
+                for (int j=0;j<=8;j++)
+                {
+                    masqueTab[i,j] = 1;
+                }
+                
+            }
+            return masqueTab;
+            
+        }
+    }
+}
